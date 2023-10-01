f762e8458bea77472957323aa8605a12c2c6ebf4
Zin Park <zin@splab.dev>
2023-10-01 14:03:12 +0900

    서블릿 등록

--- helloboot/src/main/java/tobysrping/helloboot/HellobootApplication.java
+++ helloboot/src/main/java/tobysrping/helloboot/HellobootApplication.java
@@ -1,14 +1,29 @@
 package tobysrping.helloboot;
 
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.server.WebServer;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
+
+import java.io.IOException;
 
 public class HellobootApplication {
 
 	public static void main(String[] args) {
 		ServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
-		WebServer webServer = serverFactory.getWebServer();
+		WebServer webServer = serverFactory.getWebServer(servletContext -> {
+			servletContext.addServlet("hello", new HttpServlet() {
+				@Override
+				protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+					resp.setStatus(200);
+					resp.setHeader("Content-Type", "text/plain");
+					resp.getWriter().println("Hello Servlet");
+				}
+			}).addMapping("/hello");
+		});
 		webServer.start();
 	}
 
