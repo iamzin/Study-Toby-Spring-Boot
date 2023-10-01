39f44db994d1ef6c8618567e9520a1b959d3c6f1
Zin Park <zin@splab.dev>
2023-10-01 13:47:09 +0900

    서블릿 컨테이너 띄우기

--- helloboot/src/main/java/tobysrping/helloboot/HellobootApplication.java
+++ helloboot/src/main/java/tobysrping/helloboot/HellobootApplication.java
@@ -1,9 +1,15 @@
 package tobysrping.helloboot;
 
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.server.WebServer;
+import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
+
 public class HellobootApplication {
 
 	public static void main(String[] args) {
-		System.out.println("Hello, Containerless Standalone Application");
+		ServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
+		WebServer webServer = serverFactory.getWebServer();
+		webServer.start();
 	}
 
 }
