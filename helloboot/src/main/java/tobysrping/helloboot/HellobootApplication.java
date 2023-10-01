ae07be55cced1dee5e7691c4b2ad93b003de4e7f
Zin Park <zin@splab.dev>
2023-10-01 17:01:51 +0900

    MySpringBootApplication 제거

--- helloboot/src/main/java/tobysrping/helloboot/HellobootApplication.java
+++ helloboot/src/main/java/tobysrping/helloboot/HellobootApplication.java
@@ -22,7 +22,6 @@ public class HellobootApplication {
 	}
 
 	public static void main(String[] args) {
-//		MySpringApplication.run(HellobootApplication.class, args);
 		SpringApplication.run(HellobootApplication.class, args);
 	}
 
