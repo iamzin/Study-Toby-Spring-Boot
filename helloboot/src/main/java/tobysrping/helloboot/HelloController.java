9728f30658793ba432d670c4dae37875c20f7be3
Zin Park <zin@splab.dev>
2023-10-01 14:44:37 +0900

    의존 오브젝트 추가

--- helloboot/src/main/java/tobysrping/helloboot/HelloController.java
+++ helloboot/src/main/java/tobysrping/helloboot/HelloController.java
@@ -1,7 +1,11 @@
 package tobysrping.helloboot;
 
+import java.util.Objects;
+
 public class HelloController {
     public String hello(String name) {
-        return "Hello " + name;
+        SimpleHelloService helloService = new SimpleHelloService();
+
+        return helloService.sayHello(Objects.requireNonNull(name));
     }
 }
---
+++ helloboot/src/main/java/tobysrping/helloboot/SimpleHelloService.java
@@ -0,0 +1,7 @@
+package tobysrping.helloboot;
+
+public class SimpleHelloService {
+    String sayHello(String name) {
+        return "Hello " + name;
+    }
+}
