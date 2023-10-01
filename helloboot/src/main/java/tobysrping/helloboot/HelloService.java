db418386c39c26c57131af7d85fd3e50f9863a0c
Zin Park <zin@splab.dev>
2023-10-01 14:55:21 +0900

    의존 오브젝트 DI 적용

---
+++ helloboot/src/main/java/tobysrping/helloboot/HelloService.java
@@ -0,0 +1,5 @@
+package tobysrping.helloboot;
+
+public interface HelloService {
+    String sayHello(String name);
+}
--- helloboot/src/main/java/tobysrping/helloboot/HellobootApplication.java
+++ helloboot/src/main/java/tobysrping/helloboot/HellobootApplication.java
@@ -8,7 +8,6 @@ import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactor
 import org.springframework.boot.web.server.WebServer;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 import org.springframework.context.support.GenericApplicationContext;
-import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
@@ -20,6 +19,7 @@ public class HellobootApplication {
 	public static void main(String[] args) {
 		GenericApplicationContext applicationContext = new GenericApplicationContext();
 		applicationContext.registerBean(HelloController.class);
+		applicationContext.registerBean(SimpleHelloService.class);
 		applicationContext.refresh();
 
 		ServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
