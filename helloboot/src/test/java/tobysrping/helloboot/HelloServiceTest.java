dd436eb2f7bedb31c3dc3362e26986ed16f68643
Zin Park <zin@splab.dev>
2023-10-02 13:37:17 +0900

    DI를 이용한 Decorator, Proxy 패턴

--- helloboot/src/test/java/tobysrping/helloboot/HelloServiceTest.java
+++ helloboot/src/test/java/tobysrping/helloboot/HelloServiceTest.java
@@ -3,6 +3,8 @@ package tobysrping.helloboot;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 public class HelloServiceTest {
     @Test
     void simpleHelloService() {
@@ -10,6 +12,15 @@ public class HelloServiceTest {
 
         String ret = helloService.sayHello("Test");
 
-        Assertions.assertThat(ret).isEqualTo("Hello Test");
+        assertThat(ret).isEqualTo("Hello Test");
+    }
+
+    @Test
+    void helloDecorator() {
+        HelloDecorator decorator = new HelloDecorator(name -> name);
+
+        String ret = decorator.sayHello("Test");
+
+        assertThat(ret).isEqualTo("*Test*");
     }
 }
