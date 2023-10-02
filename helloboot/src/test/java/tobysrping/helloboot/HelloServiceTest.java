019559399b25eb357608cfa8b7df0a3c86391eb2
Zin Park <zin@splab.dev>
2023-10-02 13:44:31 +0900

    메타 애노테이션과 합성 애노테이션

--- helloboot/src/test/java/tobysrping/helloboot/HelloServiceTest.java
+++ helloboot/src/test/java/tobysrping/helloboot/HelloServiceTest.java
@@ -3,10 +3,25 @@ package tobysrping.helloboot;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.METHOD)
+@UnitTest
+@interface FastUnitTest { }
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target({ElementType.ANNOTATION_TYPE, ElementType.METHOD})
+@Test
+@interface UnitTest { }
+
 public class HelloServiceTest {
-    @Test
+    @FastUnitTest
     void simpleHelloService() {
         SimpleHelloService helloService = new SimpleHelloService();
 
