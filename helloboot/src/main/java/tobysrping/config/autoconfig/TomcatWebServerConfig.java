4d850f132a9d8d80e557fce8b005a57df6ce2937
Zin Park <zin@splab.dev>
2023-10-03 12:01:07 +0900

    @Conditional과 Condition

--- helloboot/src/main/java/tobysrping/config/autoconfig/TomcatWebServerConfig.java
+++ helloboot/src/main/java/tobysrping/config/autoconfig/TomcatWebServerConfig.java
@@ -3,12 +3,24 @@ package tobysrping.config.autoconfig;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Condition;
+import org.springframework.context.annotation.ConditionContext;
+import org.springframework.context.annotation.Conditional;
+import org.springframework.core.type.AnnotatedTypeMetadata;
 import tobysrping.config.MyAutoConfiguration;
 
 @MyAutoConfiguration
+@Conditional(TomcatWebServerConfig.TomcatCondition.class)
 public class TomcatWebServerConfig {
     @Bean("tomcatWebServerFactory")
     public ServletWebServerFactory serverWebServerFactory() {
         return new TomcatServletWebServerFactory();
+    }
+
+    static class TomcatCondition implements Condition {
+        @Override
+        public boolean matches(ConditionContext context, AnnotatedTypeMetadata metadata) {
+            return false;
+        }
     }
 }
