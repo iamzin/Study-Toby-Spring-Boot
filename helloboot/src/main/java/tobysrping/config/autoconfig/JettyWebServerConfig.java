6d2241cb0529c9678e6219c8fdc98274fc702766
Zin Park <zin@splab.dev>
2023-10-03 11:54:55 +0900

    스타터와 Jetty 서버 구성 추가

--- helloboot/src/main/java/tobysrping/config/autoconfig/JettyWebServerConfig.java
+++ helloboot/src/main/java/tobysrping/config/autoconfig/JettyWebServerConfig.java
@@ -1,14 +1,14 @@
 package tobysrping.config.autoconfig;
 
-import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.embedded.jetty.JettyServletWebServerFactory;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 import org.springframework.context.annotation.Bean;
 import tobysrping.config.MyAutoConfiguration;
 
 @MyAutoConfiguration
-public class TomcatWebServerConfig {
-    @Bean("tomcatWebServerFactory")
+public class JettyWebServerConfig {
+    @Bean("jettyWebServerFactory")
     public ServletWebServerFactory serverWebServerFactory() {
-        return new TomcatServletWebServerFactory();
+        return new JettyServletWebServerFactory();
     }
 }
