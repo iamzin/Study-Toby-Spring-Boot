8038fefd1d3507a3872276c25c900f5f48204917
Zin Park <zin@splab.dev>
2023-10-15 13:37:55 +0900

    자동 구성에 Environment 프로퍼티 적용

--- helloboot/src/main/java/tobysrping/config/autoconfig/TomcatWebServerConfig.java
+++ helloboot/src/main/java/tobysrping/config/autoconfig/TomcatWebServerConfig.java
@@ -4,6 +4,7 @@ import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 import org.springframework.context.annotation.Bean;
+import org.springframework.core.env.Environment;
 import tobysrping.config.ConditionalMyOnClass;
 import tobysrping.config.MyAutoConfiguration;
 
@@ -12,7 +13,9 @@ import tobysrping.config.MyAutoConfiguration;
 public class TomcatWebServerConfig {
     @Bean("tomcatWebServerFactory")
     @ConditionalOnMissingBean
-    public ServletWebServerFactory serverWebServerFactory() {
-        return new TomcatServletWebServerFactory();
+    public ServletWebServerFactory serverWebServerFactory(Environment env) {
+        TomcatServletWebServerFactory factory = new TomcatServletWebServerFactory();
+        factory.setContextPath(env.getProperty("contextPath"));
+        return factory;
     }
 }
